8a504bd4dd81ab8f4dfe68b47b6a32e22052756b
Ellen Spertus <spertus@mills.edu>
2022-02-27 16:30:49 -0800

    Split command line

--- src/edu/mills/cs114/MillsShell.java
+++ src/edu/mills/cs114/MillsShell.java
@@ -1,7 +1,7 @@
 package edu.mills.cs114;
 
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 /**
  * A simple shell created for CS 114/214 Lecture 12.
@@ -9,25 +9,25 @@ import java.util.Scanner;
  * @author Ellen Spertus (spertus@mills.edu)
  */
 public class MillsShell {
-	/**
-	 * Prompts user for shell commands, which are executed.
-	 * 
-	 * @param args ignored
-	 * @throws IOException          if unable to get input from user
-	 * @throws InterruptedException if the command's execution is interrupted
-	 */
-	public static void main(String args[]) throws IOException, InterruptedException {
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			System.out.print("$M$ ");
-			String command = scanner.nextLine();
-			try {
-				ProcessBuilder pb = new ProcessBuilder(command);
-				Process p = pb.start();
-				p.waitFor();
-			} catch (IOException e) {
-				System.err.println(e.getMessage());
-			}
-		}
-	}
+    /**
+     * Prompts user for shell commands, which are executed.
+     * 
+     * @param args ignored
+     * @throws IOException if unable to get input from user
+     * @throws InterruptedException if the command's execution is interrupted
+     */
+    public static void main(String args[]) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("$M$ ");
+            String command = scanner.nextLine();
+            try {
+                ProcessBuilder pb = new ProcessBuilder(command.split("\\s+"));
+                Process p = pb.start();
+                p.waitFor();
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 }
