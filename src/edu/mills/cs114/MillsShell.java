3cb1fe77f847f09bb55958a61d43ada340d631b0
Ellen Spertus <spertus@mills.edu>
2022-03-01 12:52:25 -0800

    Add functionality during class

--- src/edu/mills/cs114/MillsShell.java
+++ src/edu/mills/cs114/MillsShell.java
@@ -18,12 +18,33 @@ public class MillsShell {
      */
     public static void main(String args[]) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
+        Map<String, String> env = System.getenv();
         while (true) {
             System.out.print("$M$ ");
-            String command = scanner.nextLine();
-            ProcessBuilder pb = new ProcessBuilder(command);
-            Process p = pb.start();
-            p.waitFor();
+            String[] commandWords = parseCommand(env, scanner.nextLine());
+            ProcessBuilder pb = new ProcessBuilder(commandWords);
+            try {
+                Process p = pb.start();
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                p.waitFor();
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
         }
+    }
+
+    private static String[] parseCommand(Map<String, String> env, String command) {
+        String[] commandWords = command.split("\\s+");
+        for (int i = 0; i < commandWords.length; i++) {
+            if (commandWords[i].startsWith("$")) {
+                commandWords[i] = env.getOrDefault(commandWords[i].substring(1), "");
+            }
+        }
+        return commandWords;
     }
 }
